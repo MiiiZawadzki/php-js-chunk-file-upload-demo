9e456b84fa53a585e494162eb3fe8cb9d5d5f343
MiiiZawadzki <zawadzki.micha@gmail.com>
2025-06-04 22:35:18 +0200

    add comments to server.php

--- server.php
+++ server.php
@@ -7,6 +7,7 @@ try {
     if (!is_dir($baseChunksDir)) mkdir($baseChunksDir, 0777, true);
     if (!is_dir($uploadsDir)) mkdir($uploadsDir, 0777, true);
 
+    // validate data from the request
     $uploadId = $_POST['upload_id'] ?? null;
     $filename = $_POST['filename'] ?? null;
     $fileHash = $_POST['fileHash'] ?? null;
@@ -20,16 +21,20 @@ try {
         exit;
     }
 
+    // create directory for the chunks upload
     $uploadChunkDir = $baseChunksDir . basename($uploadId) . '/';
     if (!is_dir($uploadChunkDir)) mkdir($uploadChunkDir, 0777, true);
 
     $chunkPath = "{$uploadChunkDir}" . basename($filename) . ".part{$chunkIndex}";
+
+    // upload the chunk
     if (!move_uploaded_file($file['tmp_name'], $chunkPath)) {
         http_response_code(500);
         echo json_encode(['error' => 'Nie udało się zapisać chunku.']);
         exit;
     }
 
+    // check if all chunks are received
     $allChunksReceived = true;
     for ($i = 0; $i < $totalChunks; $i++) {
         if (!file_exists("{$uploadChunkDir}{$filename}.part{$i}")) {
@@ -42,12 +47,14 @@ try {
     $message = "Chunk $currentChunk / $totalChunks saved.";
 
     if ($allChunksReceived) {
+        // create directory for the final file
         $uploadFilePath = $uploadsDir . basename($uploadId) . '/';
         if (!is_dir($uploadFilePath)) mkdir($uploadFilePath, 0777, true);
 
         $finalPath = $uploadFilePath . basename($filename);
         $output = fopen($finalPath, 'wb');
 
+        // merge all chunks into the final file
         for ($i = 0; $i < $totalChunks; $i++) {
             $chunkFilePath = "{$uploadChunkDir}{$filename}.part{$i}";
             $chunkFile = fopen($chunkFilePath, 'rb');
@@ -56,8 +63,11 @@ try {
         }
 
         fclose($output);
+
+        // delete the chunks after merging
         deleteChunks($uploadChunkDir, $filename, $totalChunks);
 
+        // verify the file hash
         $calculatedHash = hash_file('sha256', $finalPath);
 
         if ($calculatedHash !== $fileHash) {
